01cf28e58f07e4cee8b3fb79432611e020ae6f36
facundoBorssolasco <facubor26@gmail.com>
2023-06-19 18:41:08 -0300

    clase 2

--- root-proyect/src/App.jsx
+++ root-proyect/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Facundo Borssolasco</h1>
+      <h1>Facundo Borssolasco clase 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
